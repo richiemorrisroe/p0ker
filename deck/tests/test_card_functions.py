b7b513f92f11bed65af3fd6d46645702446ba2bb
richie <richie.morrisroe@gmail.com>
2020-05-05 13:39:40 +0100

    add test for count function

---
+++ deck/tests/test_card_functions.py
@@ -0,0 +1,23 @@
+from pkr import Card, Player, Suit, Rank,  Deck, deal_cards, random_hand, split_cards, count
+def test_deal_cards() -> None:
+    p1 = Player()
+    p2 = Player()
+    list_players = [p1, p2]
+    d = Deck()
+    cards_in_hand = 5
+    d, p = deal_cards(d, list_players)
+    p1, p2 = p
+    assert len(p1.hand)==5
+
+def test_split_cards() -> None:
+    rhand = random_hand()
+    ranks, suits = split_cards(rhand)
+    assert len(ranks) and len(suits) == 5
+
+def test_count() -> None:
+    hand = [Card(Suit(1), Rank(14)), Card(Suit(2), Rank(14)),
+            Card(Suit(3), Rank(14)), Card(Suit(1), Rank(8)),
+            Card(Suit(1), Rank(8))]
+    ranks, suits = split_cards(hand)
+    count_ranks = count(ranks)
+    assert max(count_ranks.values()) == 3
