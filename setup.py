4880f6931cade03454fab9c817495c524055b964
Richie Morrisroe <richie.morrisroe@gmail.com>
2020-12-18 11:46:58 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -18,5 +18,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires = [
+        'hypothesis'],
     python_requires='>=3.6',
 )
