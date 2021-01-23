1c2927c94922ab18ef3b1b1ba082883015229895
Richie Morrisroe <richie.morrisroe@gmail.com>
2021-01-23 16:11:36 +0000

    fix code with black

--- deck/tests/test_cards.py
+++ deck/tests/test_cards.py
@@ -1,4 +1,4 @@
-# type: ignore 
+# type: ignore
 import pytest
 from deck.pkr import Rank, Suit, Card, Round
 
@@ -12,9 +12,11 @@ def generate_suit(num) -> Suit:
     s = Suit(num)
     return s
 
+
 def test_suit_min() -> None:
     with pytest.raises(ValueError):
         suit = generate_suit(0)
+
 
 def test_suit_max() -> None:
     with pytest.raises(ValueError):
@@ -25,6 +27,7 @@ def test_rank_min() -> None:
     with pytest.raises(ValueError):
         rank = generate_rank(1)
 
+
 def test_rank_max() -> None:
     with pytest.raises(ValueError):
         rank = generate_rank(15)
@@ -33,40 +36,48 @@ def test_rank_max() -> None:
 # ace_of_spades = Card(Suit(1), Rank(14))
 # def test_suit_and_rank():
 #     assert (ace_of_spades == Card(Suit(1), Rank(14)))
-    
+
 Ace = Rank(14)
 Deuce = Rank(2)
 
+
 def test_rank_ordering() -> None:
     assert Ace > Deuce
+
 
 def test_wrong_rank_ordering() -> None:
     with pytest.raises(AssertionError):
         assert Deuce > Ace
 
+
 def test_court_cards() -> None:
     assert Rank(13) > Rank(12) > Rank(11)
+
 
 def test_card_equality() -> None:
     c1 = Card(Rank(14), Suit(1))
     c2 = Card(Rank(14), Suit(1))
     assert c1 == c2
 
+
 def test_card_notequal() -> None:
     c1 = Card(Rank(14), Suit(1))
     c2 = Card(Rank(14), Suit(2))
     assert c1 != c2
 
+
 def test_card_wrong_order_fails() -> None:
     with pytest.raises(AssertionError):
         Card(Suit(1), Rank(2))
+
 
 def test_card_greater_than() -> None:
     c1 = Card(Rank(14), Suit(1))
     c2 = Card(Rank(13), Suit(2))
     assert c1 > c2
 
+
 def test_card_less_than() -> None:
     c1 = Card(Rank(14), Suit(1))
     c2 = Card(Rank(13), Suit(2))
-    assert c2  <   c1
+    assert c2 < c1
--- deck/tests/test_score_hand.py
+++ deck/tests/test_score_hand.py
@@ -1,45 +1,83 @@
-from deck.pkr import Card, Suit, Rank, Hand,  make_straight, Round
+from deck.pkr import Card, Suit, Rank, Hand, make_straight, Round
 from deck.stats import generate_hands
+
+
 def test_score_full_house() -> None:
-    full_house = Hand([Card(Rank(14), Suit(1)), Card(Rank(14),Suit(2)),
-                       Card(Rank(14), Suit(3)), Card(Rank(8),Suit(1)),
-                       Card(Rank(8),Suit(2))])
+    full_house = Hand(
+        [
+            Card(Rank(14), Suit(1)),
+            Card(Rank(14), Suit(2)),
+            Card(Rank(14), Suit(3)),
+            Card(Rank(8), Suit(1)),
+            Card(Rank(8), Suit(2)),
+        ]
+    )
     score, name = full_house.score()
-    assert name == 'FULL-HOUSE'
+    assert name == "FULL-HOUSE"
+
 
 def test_score_pair() -> None:
-    pair = Hand([Card(Rank(8),Suit(1)), Card(Rank(8), Suit(2)),
-                 Card(Rank(2), Suit(1)), Card( Rank(3), Suit(2)),
-                 Card(Rank(5), Suit(3))])
+    pair = Hand(
+        [
+            Card(Rank(8), Suit(1)),
+            Card(Rank(8), Suit(2)),
+            Card(Rank(2), Suit(1)),
+            Card(Rank(3), Suit(2)),
+            Card(Rank(5), Suit(3)),
+        ]
+    )
     score, name = pair.score()
-    assert name == 'PAIR'
-    
+    assert name == "PAIR"
+
+
 def test_score_straight() -> None:
     straight = make_straight(start=5)
     score, name = straight.score()
     ## make straight sometimes returns a straight flush
-    assert name.startswith('STRAIGHT')
+    assert name.startswith("STRAIGHT")
+
 
 def test_score_straight_flush() -> None:
-    straight_flush = Hand([Card( Rank(7),Suit(1)),  Card(Rank(8),Suit(1)),
-                           Card(Rank(9), Suit(1)), Card( Rank(10), Suit(1)),
-                           Card(Rank(11), Suit(1))])
+    straight_flush = Hand(
+        [
+            Card(Rank(7), Suit(1)),
+            Card(Rank(8), Suit(1)),
+            Card(Rank(9), Suit(1)),
+            Card(Rank(10), Suit(1)),
+            Card(Rank(11), Suit(1)),
+        ]
+    )
     score, name = straight_flush.score()
-    assert name == 'STRAIGHT-FLUSH'
+    assert name == "STRAIGHT-FLUSH"
+
 
 def test_score_three_of_a_kind() -> None:
-    three_of_a_kind = Hand([Card(Rank(14), Suit(1)), Card( Rank(14), Suit(2)),
-                            Card(Rank(14), Suit(3)), Card( Rank(3), Suit(1)),
-                            Card(Rank(5), Suit(1))])
+    three_of_a_kind = Hand(
+        [
+            Card(Rank(14), Suit(1)),
+            Card(Rank(14), Suit(2)),
+            Card(Rank(14), Suit(3)),
+            Card(Rank(3), Suit(1)),
+            Card(Rank(5), Suit(1)),
+        ]
+    )
     score, name = three_of_a_kind.score()
-    assert name == 'THREE-OF-A-KIND'
+    assert name == "THREE-OF-A-KIND"
+
 
 def test_score_twopair() -> None:
-    twopair = Hand([Card(Rank(8), Suit(1)), Card(Rank(8), Suit(2)),
-                    Card(Rank(2), Suit(1)), Card( Rank(2), Suit(2)),
-                    Card(Rank(5), Suit(3))])
+    twopair = Hand(
+        [
+            Card(Rank(8), Suit(1)),
+            Card(Rank(8), Suit(2)),
+            Card(Rank(2), Suit(1)),
+            Card(Rank(2), Suit(2)),
+            Card(Rank(5), Suit(3)),
+        ]
+    )
     score, name = twopair.score()
-    assert name == 'TWO-PAIR'
+    assert name == "TWO-PAIR"
+
 
 def test_all_hands_can_be_scored() -> None:
     n = 1000
